6622d7a22b213265de241fa34b4096f04fdce879
Dan Sowter <dan@netengine.com.au>
2013-03-08 16:24:04 +1000

    Pulls out unused base API controller error-handling

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -7,20 +7,7 @@ class Api::V1::BaseController < ActionController::Base
     @user
   end
 
-  rescue_from ::ActiveRecord::RecordNotFound do
-    respond_with_error!(:not_found, status: :not_found)
-  end
-
   private
-
-  def respond_with_error!(error, options = {})
-    status = options.delete(:status) || :bad_request
-    location = options.delete(:location) || root_path
-    response = ({ error: error, message: t("api.error.#{error}", options.delete(:message_params)) }).merge!(options)
-    respond_to do |format|
-      format.json { render json: response.to_json, status: status, location: location }
-    end
-  end
 
   def restrict_access
     authenticate_or_request_with_http_token do |token, options|
