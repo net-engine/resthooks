7f3dc3ebb98dca8e193229ec0be6f732d272ee98
Dan Sowter <dan@netengine.com.au>
2013-03-08 12:12:47 +1000

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Resthooks::Application.config.secret_key_base = '7cfeda03863c44ee1abcb347a6f3a973de03f1f88765a68b670eb1010526814603effe34562ec1efea446dd353d823ce54c10bf8eb2666c843d862dd6c183b04'
