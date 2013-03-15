821e996cdcb4568f5718e1141a8cf8dc5fb5386f
Ricardo Bernardeli <ricardobcs@gmail.com>
2013-03-15 15:43:47 +1000

    Adds email/password to factory to fix tests.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,7 @@ FactoryGirl.define do
   factory :user do
     firstname { Faker::Name.first_name }
     lastname { Faker::Name.last_name }
+    email { Faker::Internet.email }
+    password 'password123'
   end
 end
